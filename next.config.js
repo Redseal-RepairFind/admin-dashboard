1fa04ae829b36d4c1a34494b0cbe662b2ff54eff
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2024-06-01 22:26:25 +0100

    Updated customers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["saheedwale.s3.amazonaws.com"],
+    domains: [
+      "saheedwale.s3.amazonaws.com",
+      "ipalas3bucket.s3.us-east-2.amazonaws.com",
+    ],
   },
 };
 
