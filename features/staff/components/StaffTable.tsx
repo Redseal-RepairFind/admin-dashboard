3897c958f3e9c642bb381fe769e52927a7788377
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2024-06-10 07:37:15 +0100

    Updated Employees

--- features/staff/components/StaffTable.tsx
+++ features/staff/components/StaffTable.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import TableCard from "@/features/shared/table/components/table-card";
 import Heading from "@/features/shared/table/components/table-heading";
 import Searchbar from "@/features/shared/table/components/searchbar";
@@ -13,19 +13,82 @@ import Td from "@/features/shared/table/components/td";
 import { RatingStar } from "@/public/svg";
 import { formatDateToDDMMYY } from "@/lib/utils/format-date";
 // import FilterBox from "./filter-box";
-import { useCustomersTable } from "../../customers/hooks/table";
+import { permissions } from "lib/api/permissions";
+import { useQuery } from "react-query";
 import useStaff from "@/lib/hooks/useStaff";
+import { Modal } from "react-responsive-modal";
+import AddStaff from "./AddStaff";
+import "react-responsive-modal/styles.css";
+import VerticalMenu from "@/components/shared/vertical-menu";
+import EditPermissions from "./EditPermissions";
+import toast from "react-hot-toast";
 
-const table_headings = ["Staff Name", "Date Joined", "Email Address", "Status"];
+const table_headings = [
+  "Staff Name",
+  "Date Joined",
+  "Email Address",
+  "Status",
+  "Action",
+];
 
 interface IProps {
   setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const CustomersTable: React.FC<IProps> = ({ setLoading }) => {
-  const { staffData, loadingStaff } = useStaff();
+  const { staffData, SuspendStaff } = useStaff();
 
-  console.log(staffData);
+  const [open, setOpen] = useState(false);
+  const [openPermissions, setOpenPermissions] = useState(false);
+
+  const [currentStaff, setCurrentStaff] = useState();
+
+  // console.log(staffData?.admins);
+  // console.log(permissionList);
+
+  const modalRef = useRef(null);
+  const permissionRef = useRef(null);
+
+  let rowOptions = [
+    {
+      name: "Edit Permissions",
+      action: (item: any) => {
+        // console.log(item);
+        setCurrentStaff(item);
+        setOpenPermissions(true);
+      },
+    },
+    {
+      name: "Suspend/Activate",
+      action: async (item: any) => {
+        if (
+          confirm(
+            `Are you sure you wish to ${
+              item?.status === "ACTIVE" ? "suspend" : "activate"
+            } this user?`
+          )
+        ) {
+          toast.loading("Processing...");
+          const payload = {
+            staffId: item?._id,
+            status: item?.status === "ACTIVE" ? "SUSPENDED" : "ACTIVE",
+          };
+          try {
+            const data = await SuspendStaff(payload);
+            toast.remove();
+            toast.success(data?.message);
+            setTimeout(() => {
+              location.reload();
+            }, 1000);
+          } catch (e: any) {
+            console.log(e);
+            toast.remove();
+            toast.error(e?.response?.data?.message);
+          }
+        }
+      },
+    },
+  ];
 
   return (
     <TableCard>
@@ -47,7 +110,39 @@ const CustomersTable: React.FC<IProps> = ({ setLoading }) => {
             />
           </Filter> */}
         </div>
+        <button
+          onClick={() => setOpen(true)}
+          className="border border-black bg-black text-white py-2.5 px-5 rounded-md"
+        >
+          Create Employee
+        </button>
       </div>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        center
+        classNames={{
+          modal: "customModal",
+        }}
+        container={modalRef.current}
+      >
+        <div className="w-[600px] pt-6">
+          <AddStaff />
+        </div>
+      </Modal>
+      <Modal
+        open={openPermissions}
+        onClose={() => setOpenPermissions(false)}
+        center
+        classNames={{
+          modal: "customModal",
+        }}
+        container={permissionRef.current}
+      >
+        <div className="w-[600px] pt-6">
+          <EditPermissions currentStaff={currentStaff} />
+        </div>
+      </Modal>
 
       <TableOverflow>
         <Table>
@@ -60,17 +155,35 @@ const CustomersTable: React.FC<IProps> = ({ setLoading }) => {
           </Thead>
 
           <tbody>
-            {/* {staffData?.map((item:any, index:number) => (
-              <tr
-                key={item.customer._id}
-                className="cursor-pointer"
-              >
-                <Td>{item?.firstName}{' '}{item?.lastName}</Td>
+            {staffData?.admins?.map((item: any, index: number) => (
+              <tr key={item?._id} className="cursor-pointer">
+                <Td>
+                  {item?.firstName} {item?.lastName}
+                </Td>
                 <Td>{formatDateToDDMMYY(item?.createdAt)}</Td>
                 <Td>{item?.email}</Td>
                 <Td>{item?.status}</Td>
+                <Td>
+                  <div onClick={(e) => e.stopPropagation()} className="w-fit">
+                    <VerticalMenu isBackground={true}>
+                      <div>
+                        {rowOptions?.map((option, index) => (
+                          <button
+                            key={index}
+                            onClick={() => {
+                              option?.action(item);
+                            }}
+                            className="block w-full border border-slate-100 px-4 py-2 text-left bg-white duration-200 text-baseFont text-gray-700 hover:bg-gray-100 hover:text-gray-900 cursor-pointer"
+                          >
+                            {option?.name}
+                          </button>
+                        ))}
+                      </div>
+                    </VerticalMenu>
+                  </div>
+                </Td>
               </tr>
-            ))} */}
+            ))}
           </tbody>
         </Table>
       </TableOverflow>
--- lib/hooks/useStaff.ts
+++ lib/hooks/useStaff.ts
@@ -1,13 +1,30 @@
+import React, { useState } from "react";
 import { staff } from "../api/staff";
+import { permissions } from "../api/permissions";
 import { useMutation, useQuery } from "react-query";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
 const useStaff = () => {
+  const { mutateAsync: AddStaff } = useMutation(staff.addStaff);
+  const { mutateAsync: AddPermission } = useMutation(staff.addStaffPermission);
+  const { mutateAsync: RemovePermission } = useMutation(
+    staff.removeStaffPermission
+  );
+  const { mutateAsync: SuspendStaff } = useMutation(staff.suspendStaff);
+
   const { data: staffData, isLoading: loadingStaff } = useQuery(
     ["Staff"],
     () => {
       return staff.getStaff();
+    },
+    { cacheTime: 30000, staleTime: 30000, refetchOnWindowFocus: true }
+  );
+
+  const { data: permissionList, isLoading: loadingPermissions } = useQuery(
+    ["Permissions"],
+    () => {
+      return permissions.getPermissions();
     },
     { cacheTime: 30000, staleTime: 30000, refetchOnWindowFocus: true }
   );
@@ -17,6 +34,11 @@ const useStaff = () => {
   return {
     staffData,
     loadingStaff,
+    permissionList: permissionList?.permissions,
+    AddStaff,
+    AddPermission,
+    RemovePermission,
+    SuspendStaff,
   };
 };
 
