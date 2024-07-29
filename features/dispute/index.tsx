331a47fe19d333c81bff401584a17f019f31e207
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2024-07-29 13:47:28 +0100

    Feat: Updated dispute

--- features/dispute/index.tsx
+++ features/dispute/index.tsx
@@ -6,20 +6,14 @@ import PageHeading from "../shared/page-body/page-heading";
 import DownloadButton from "../shared/page-body/download-button";
 import LoadingTemplate from "../layout/loading";
 import DisputeTable from "./components/DisputeTable";
-import { LoadScript } from "@react-google-maps/api";
 
 const Index = () => {
   return (
     <>
-      <LoadScript
-        googleMapsApiKey={`${process.env.NEXT_PUBLIC_MAP_API_KEY}`}
-        libraries={["places"]}
-      >
-        <Header />
-        <PageBody>
-          <DisputeTable />
-        </PageBody>
-      </LoadScript>
+      <Header />
+      <PageBody>
+        <DisputeTable />
+      </PageBody>
     </>
   );
 };
