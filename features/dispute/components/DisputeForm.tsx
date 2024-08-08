bedfc081cd924156a937bef629d738f613f8b5e4
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2024-08-08 15:38:19 +0100

    Feat: Updated Form & Media

--- features/dispute/components/DisputeForm.tsx
+++ features/dispute/components/DisputeForm.tsx
@@ -1,33 +1,16 @@
 "use client";
-import React, { useState } from "react";
-import {
-  FaCaretDown,
-  FaCaretUp,
-  FaLocationArrow,
-  FaCalendar,
-} from "react-icons/fa";
+import React from "react";
+import SectionContainer from "./SectionContainer";
 
 const DisputeForm = ({ info }: { info: any }) => {
-  const [showData, setShowData] = useState(false);
-
-  const handleToggle = () => setShowData(!showData);
-
   return (
-    <div className="p-5 bg-white rounded-lg my-4">
-      <div className="flex items-center justify-between">
-        <h1 className="font-semibold">Dispute Form</h1>
-        <button onClick={handleToggle}>
-          {showData ? <FaCaretUp /> : <FaCaretDown />}
-        </button>
-      </div>
-      {showData && (
-        <div className="w-full mt-4">
-          <div className="p-4 bg-gray-100 text-sm font-medium rounded-md">
-            {info}
-          </div>
+    <SectionContainer title="Dispute Form">
+      <div className="w-full mt-4 pb-5">
+        <div className="p-4 bg-gray-100 text-sm font-medium rounded-md">
+          {info}
         </div>
-      )}
-    </div>
+      </div>
+    </SectionContainer>
   );
 };
 
