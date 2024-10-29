41c0788e933a77c0dd93ddc98f98f9299e5ad57c
themystic1 <cluckygo@gmail.com>
2024-10-29 22:21:55 +0100

    ok

---
+++ lib/hooks/usePromotion.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect, useMemo } from "react";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { promotions } from "../api/promotions";
+import { useQuery, useMutation } from "react-query";
+import { formatDate } from "@/lib/utils/format-date";
+
+export function usePromotion() {
+  const { mutateAsync: CreatePromo } = useMutation(promotions.createPromo);
+  const { mutateAsync: UpdatePromo } = useMutation(promotions.updatePromo);
+
+  const { mutateAsync: deletePromo } = useMutation(promotions.deletePromo);
+
+  const {
+    data: Promotions,
+    isLoading: loadingPromo,
+    refetch: refetchPromo,
+  } = useQuery(
+    ["promotions"],
+    () => {
+      return promotions.getPromo();
+    },
+    { cacheTime: 30000, staleTime: 30000, refetchOnWindowFocus: true }
+  );
+
+  return {
+    Promotions,
+    loadingPromo,
+    refetchPromo,
+    CreatePromo,
+    UpdatePromo,
+    deletePromo,
+  };
+}
