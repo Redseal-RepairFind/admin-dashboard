8c7467b2ef0d82d5265b84f04dba9f20cd2b4700
themystic1 <cluckygo@gmail.com>
2024-10-30 07:22:11 +0100

    skills rendered

--- features/customise/components/tabs.tsx
+++ features/customise/components/tabs.tsx
@@ -56,7 +56,7 @@ const Tabs: React.FC = () => {
     if (newSkills.includes("")) {
       return toast.error("Please fill all empty skill inputs");
     }
-    toast.loading("Processing new skill");
+    toast.loading("Processing new skill(s)...");
 
     let data;
     try {
@@ -64,11 +64,10 @@ const Tabs: React.FC = () => {
         data = await AddSkill({ name: skillInputs[0].value });
       } else if (skillInputs.length > 1) {
         data = await AddSkills({ skills: newSkills });
-
-        console.log(newSkills, newSkills.length);
       }
       toast.remove();
       toast.success(data?.message);
+      setSkillInputs([{ id: 0, value: "" }]);
       setTimeout(() => {
         refetchSkills();
       }, 1000);
@@ -142,76 +141,78 @@ const Tabs: React.FC = () => {
         {/* <button
           className={`px-6 py-2 text-sm rounded transition-all duration-300 ${
             activeTab === 4
-              ? "bg-white border border-[#262626] text-[#262626]"
+            ? "bg-white border border-[#262626] text-[#262626]"
               : "bg-white"
           }`}
           onClick={() => handleTabChange(4)}
         >
           Promotion
-        </button> */}
+          </button> */}
       </div>
       <div className="mt-2">
         {activeTab === 1 && <Quiz />}
         {activeTab === 2 && <EditQuizTab />}
         {activeTab === 3 && (
-          <div className="w-[50%] pt-10">
-            <Skills />
-            <div className="mt-4">
-              <label
-                htmlFor="username"
-                className="block font-medium leading-6 text-gray-900 mb-4"
-              >
-                New skill
-              </label>
+          <div className="grid grid-cols-2">
+            <div className="w-full pt-10">
+              <div className="mt-4">
+                <label
+                  htmlFor="username"
+                  className="block font-medium leading-6 text-gray-900 mb-4"
+                >
+                  New skill
+                </label>
 
-              {skillInputs.map((input, i) => (
-                <div className="flex items-center gap-4" key={i}>
-                  <input
-                    type="text"
-                    name="skill"
-                    id={`skill-${i}`}
-                    autoComplete="skill"
-                    className="w-[100%] border-0 py-2 px-3 mt-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 capitalize"
-                    placeholder="Add new skill"
-                    value={input.value}
-                    onChange={(e) => handleSkillChange(i, e.target.value)}
-                  />
-                  {i !== 0 ? (
-                    <button
-                      onClick={() => removeSkillInput(i)}
-                      className="font-bold text-3xl"
-                    >
-                      &times;
-                    </button>
-                  ) : null}
-                </div>
-              ))}
-            </div>
-            <button
-              onClick={addSkillInput}
-              className="text-gray-600 mr-6 capitalize border border-[#262626] py-1.5 px-6 rounded-md"
-            >
-              + new skill
-            </button>
-            <button
-              className="border-0 bg-[#262626] text-[#fff] px-6 py-2 rounded mt-10 text-sm hover:opacity-90 hover:scale-[0.99] transition-all"
-              onClick={submitNewSkill}
-            >
-              Publish Skills
-            </button>
+                {skillInputs.map((input, i) => (
+                  <div className="flex items-center gap-4" key={i}>
+                    <input
+                      type="text"
+                      name="skill"
+                      id={`skill-${i}`}
+                      autoComplete="skill"
+                      className="w-[60%] border-0 py-2 px-3 mt-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 capitalize"
+                      placeholder="Add new skill"
+                      value={input.value}
+                      onChange={(e) => handleSkillChange(i, e.target.value)}
+                    />
+                    {i !== 0 ? (
+                      <button
+                        onClick={() => removeSkillInput(i)}
+                        className="font-bold text-3xl"
+                      >
+                        &times;
+                      </button>
+                    ) : null}
+                  </div>
+                ))}
+              </div>
+              <div className="flex mt-10 items-center gap-2">
+                <button
+                  onClick={addSkillInput}
+                  className="text-gray-600 mr-6 capitalize border border-[#262626] py-1.5 px-6 rounded-md"
+                >
+                  + new skill
+                </button>
+                <button
+                  className="border-0 bg-[#262626] text-[#fff] px-6 py-2 rounded text-sm hover:opacity-90 hover:scale-[0.99] transition-all"
+                  onClick={submitNewSkill}
+                >
+                  Publish Skills
+                </button>
+              </div>
 
-            {/* <div className="mt-6">
+              {/* <div className="mt-6">
           <h1 className="font-bold text-2xl mb-4">All Skills</h1>
           {skillsToRender?.map((skill: any, i: number) => (
             <div className="flex items-center gap-2 mb-4" key={i}>
-              <span className="flex justify-center rounded-full items-center h-6 w-6 bg-white text-black text-sm">
-                {i + 1}
-              </span>
-
-              <div className="grid grid-cols-[600px,100px] relative">
-                <Modal isOpen={modalOpen} onClose={closeModal}>
-                  <div className="w-24">
-                    <input
+            <span className="flex justify-center rounded-full items-center h-6 w-6 bg-white text-black text-sm">
+            {i + 1}
+            </span>
+            
+            <div className="grid grid-cols-[600px,100px] relative">
+            <Modal isOpen={modalOpen} onClose={closeModal}>
+            <div className="w-24">
+            <input
                       type="text"
                       name="skill"
                       autoComplete="skill"
@@ -225,20 +226,20 @@ const Tabs: React.FC = () => {
                 <p className="text-sm ">{skill?.name}</p>
                 <span>
                   <button
-                    onClick={() => setOpenMenu(openMenu === i ? null : i)} // Toggle only the current skill's menu
+                  onClick={() => setOpenMenu(openMenu === i ? null : i)} // Toggle only the current skill's menu
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
+                  <svg
+                  xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
                       width="24"
                       height="24"
                       fill="currentColor"
-                    >
+                      >
                       <circle cx="12" cy="5" r="2" />
                       <circle cx="12" cy="12" r="2" />
                       <circle cx="12" cy="19" r="2" />
-                    </svg>
-                  </button>
+                      </svg>
+                      </button>
                   {openMenu === i ? ( // Show menu only if openMenu matches the current index
                     <div className="absolute right-0 top-0 bg-gray-100 h-14 w-20 shadow-md">
                       <button
@@ -255,12 +256,14 @@ const Tabs: React.FC = () => {
             </div>
           ))}
           <button
-            className="border-0 bg-[#262626] text-[#fff] px-6 py-2 rounded mt-10 text-sm hover:opacity-90 hover:scale-[0.99] transition-all mb-6"
-            onClick={() => setIsFulSkill((is) => !is)}
+          className="border-0 bg-[#262626] text-[#fff] px-6 py-2 rounded mt-10 text-sm hover:opacity-90 hover:scale-[0.99] transition-all mb-6"
+          onClick={() => setIsFulSkill((is) => !is)}
           >
-            {isFulSkill ? "Show less" : "Show all"}
+          {isFulSkill ? "Show less" : "Show all"}
           </button>
-        </div> */}
+          </div> */}
+            </div>
+            <Skills />
           </div>
         )}
         {/* {activeTab === 4 && <Promotion />} */}
