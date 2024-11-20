4474c9f4aa6212ac077aecec9628b5cf6abb0be7
themystic1 <cluckyugo@gmail.com>
2024-11-20 08:52:18 +0100

    @

--- app/_components/ContractorLink.tsx
+++ app/_components/ContractorLink.tsx
@@ -7,8 +7,6 @@ export default function ContractorProfileLink({
 }: {
   contractorsId: string;
 }) {
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-
   useEffect(() => {
     const openProfile = () => {
       // Detect global object
@@ -24,9 +22,7 @@ export default function ContractorProfileLink({
       if (!globalObject || !globalObject.location) {
         // Fallback if no global object with location
         console.error("No global object with location support.");
-        setErrorMessage(
-          "Your browser does not support deep linking. Please visit the App Store or Play Store."
-        );
+
         return;
       }
 
@@ -44,16 +40,10 @@ export default function ContractorProfileLink({
 
       // Attempt to open the app
       const startTime = Date.now();
-      try {
-        globalObject.location.href = appURL;
-      } catch (error) {
-        console.error("Error attempting to open the app:", error);
-        setErrorMessage("Failed to open the app. Please try again.");
-        return;
-      }
+      globalObject.location.href = appURL;
 
-      // Redirect after timeout if the app isn't opened
-      const timeout = setTimeout(() => {
+      // Redirect to fallback if the app is not opened
+      setTimeout(() => {
         if (Date.now() - startTime < 1500) {
           if (isIOS) {
             globalObject.location.href = appStoreURL;
@@ -64,29 +54,10 @@ export default function ContractorProfileLink({
           }
         }
       }, 1500);
-
-      // Cleanup timeout
-      return () => clearTimeout(timeout);
     };
 
     openProfile(); // Automatically trigger on mount
   }, [contractorsId]);
 
-  return (
-    <>
-      {errorMessage && (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white p-6 rounded shadow-lg">
-            <p className="text-red-600 font-bold mb-4">{errorMessage}</p>
-            <a
-              href="https://repairfind.ca/"
-              className="text-blue-500 underline"
-            >
-              Open Website
-            </a>
-          </div>
-        </div>
-      )}
-    </>
-  );
+  return null;
 }
