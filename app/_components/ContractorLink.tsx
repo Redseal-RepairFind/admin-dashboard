0f1900be799281bb5d3288f8659ee26d587eb4d6
themystic1 <cluckyugo@gmail.com>
2024-11-20 09:33:00 +0100

    changes

--- app/_components/ContractorLink.tsx
+++ app/_components/ContractorLink.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export default function ContractorProfileLink({
   contractorsId,
@@ -9,34 +9,14 @@ export default function ContractorProfileLink({
 }) {
   useEffect(() => {
     const openProfile = () => {
-      // Detect global object
-      const globalObject =
-        typeof window !== "undefined"
-          ? window
-          : typeof self !== "undefined"
-          ? self
-          : typeof global !== "undefined"
-          ? global
-          : null;
-
-      if (!globalObject || !globalObject.location) {
-        // Fallback if no global object with location
-        console.error("No global object with location support.");
-
-        return;
-      }
-
-      // Define URLs
       const appURL = `repairfindcustomerapp://contractorprofile?contractorsId=${contractorsId}`;
       const appStoreURL = "https://apps.apple.com/app/6483208887";
       const playStoreURL =
         "https://play.google.com/store/apps/details?id=com.theproductdude.repairfindcustomer&pli=1";
       const fallbackURL = `https://repairfind.ca/`;
 
-      // Detect platform
-      const userAgent = globalObject.navigator?.userAgent || "";
-      const isIOS = /iPhone|iPad|iPod/i.test(userAgent);
-      const isAndroid = /Android/i.test(userAgent);
+      const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+      const isAndroid = /Android/i.test(navigator.userAgent);
 
       // Attempt to open the app
       const startTime = Date.now();
@@ -45,18 +25,20 @@ export default function ContractorProfileLink({
       // Redirect to fallback if the app is not opened
       setTimeout(() => {
         if (Date.now() - startTime < 1500) {
-          // if (isIOS) {
-          //   window.location.href = appStoreURL;
-          // } else if (isAndroid) {
-          //   window.location.href = playStoreURL;
-          // } else {
-          window.location.href = fallbackURL; // Desktop fallback
-          // }
+          if (isIOS && !isAndroid) {
+            window.location.href = appStoreURL;
+          } else if (!isIOS && isAndroid) {
+            window.location.href = playStoreURL;
+          }
+        } else {
+          // If the app isn't opened within 1500ms, redirect to fallback URL
+          window.location.href = fallbackURL;
         }
       }, 1500);
     };
 
     openProfile(); // Automatically trigger on mount
   }, [contractorsId]);
-  return null;
+
+  return null; // No UI since it auto-triggers
 }
